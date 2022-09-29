791815f812e99c183777597600fa791aada50164
AlexNathanson <alex@alexnathanson.com>
2022-09-29 11:02:42 -0400

    psuedo code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # smartDCloadcenter
-smart DC load center for off-grid renewable energy systems
+smart DC load center for managing intermittency in off-grid renewable energy systems
 
 
 <img src="schematic/schematic-sept29.png">
--- python/smart-load-manager.py
+++ python/smart-load-manager.py
@@ -2,7 +2,9 @@
 
 
 '''
-psuedo code
+Data to store in advance
+* regional insolation data
+* regional rain data
 
 Data to collect
 1) historic production data from CC
@@ -11,6 +13,20 @@ Data to collect
 4) user data about load priorities
 	a) which are crucial loads and which are ideal loads
 	b) which are time-independent/asyncronous and which are time specific
+
+
+predict w/o weather forcast:
+* estimate energy availability for that day
+* runs async processes when energy is plentiful and curtailment is happenings
+
+
+
+For each load:
+Get power draw over time, determine patterns of activity
+
+
+If internet i.e. weather forcasts are available:
+* estimate energy availabilty for coming days
 '''
 
 
